bd7ab6dd921b9ed3f1725b0dc25fdca67dc85dd4
Nicholas Delpino <NBDelpino4@gmail.com>
2018-05-14 14:36:50 -0400

    functions well, styles added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,83 @@
 var heads = 0;
 var tails = 0;
 var flips = heads + tails;
+var sound = document.getElementById("coinSound");
 
-// Onclick handler in HTML triggers function
-// Hide static coin, Get random result, set image to heads if < .5, tails if otherwise, increment count then display count
+// Onclick handler located in HTML
+
 function coinToss (){
 
-        document.getElementById("static-coin").style.display = 'none';
+    //toggle reset button
 
-    if(Math.random() > .5){
-        document.getElementById("coin").src = "assets/images/heads.png" //place heads coin image path here
-        heads ++;
-        flips ++;
-        document.getElementById("heads").innerHTML = heads;
-        document.getElementById("total-flips").innerHTML = flips;
-    }
-    else {
-        document.getElementById("coin").src ="assets/images/tails.png" //place tails coin image path here
-        tails ++;
-        flips ++;
-        document.getElementById("tails").innerHTML = tails;
-        document.getElementById("total-flips").innerHTML = flips;
-    }
+    document.getElementById("reset-btn").src ="assets/images/reset-out.png";;
+
+    //toggle coin
+
+    document.getElementById("static-coin").style.display = 'none';
+    document.getElementById("coin").style.display = 'none';
+    document.getElementById("flipping-coin").style.display = 'block';
+
+    //play sound
+
+    sound.play();
+
+    //delay actions to coincide with audio ending
+
+    setTimeout(function(){
+
+        //toggle coin
+
+        document.getElementById("flipping-coin").style.display = 'none';
+
+        //toggle coin
+
+        document.getElementById("coin").style.display = 'block';
+
+        //toggle button
+
+        document.getElementById("button").src ="assets/images/btn-out.png";
+
+        //calculate then display result - matching coin and count
+
+        if(Math.random() > .5){
+            document.getElementById("coin").src = "assets/images/heads.png" //place heads coin image path here
+            heads ++;
+            flips ++;
+            document.getElementById("heads").innerHTML = heads;
+            document.getElementById("total-flips").innerHTML = flips;;
+        }
+        else {
+            document.getElementById("coin").src ="assets/images/tails.png" //place tails coin image path here
+            tails ++;
+            flips ++;
+            document.getElementById("tails").innerHTML = tails;
+            document.getElementById("total-flips").innerHTML = flips;
+        }
+    }, 700);
+
+    //toggle button
+
+     document.getElementById("button").src ="assets/images/btn-in.png";
+
 }
+
+
+//restart everything
+
+function restart(){
+    document.location.href = "";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
