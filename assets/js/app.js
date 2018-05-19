876742548b129040dfcb2e66b9cacd88b9b58878
Nicholas Delpino <NBDelpino4@gmail.com>
2018-05-19 18:38:31 -0400

    refined styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ var heads = 0;
 var tails = 0;
 var flips = heads + tails;
 var sound = document.getElementById("coinSound");
+var sound2 = document.getElementById("reset-button-sound-effect");
 
 // Onclick handler located in HTML
 
@@ -9,13 +10,13 @@ function coinToss (){
 
     //toggle reset button
 
-    document.getElementById("reset-btn").src ="assets/images/reset-out.png";;
+    document.getElementById("reset-btn").src ="assets/images/reset-button-out.svg";
 
     //toggle coin
 
     document.getElementById("static-coin").style.display = 'none';
     document.getElementById("coin").style.display = 'none';
-    document.getElementById("flipping-coin").style.display = 'block';
+    document.getElementById("flipping-coin").style.display = 'inline';
 
     //play sound
 
@@ -31,23 +32,23 @@ function coinToss (){
 
         //toggle coin
 
-        document.getElementById("coin").style.display = 'block';
+        document.getElementById("coin").style.display = 'inline';
 
         //toggle button
 
-        document.getElementById("button").src ="assets/images/btn-out-new.png";
+        document.getElementById("button").src ="assets/images/main-button-out.svg";
 
         //calculate then display result - matching coin and count
 
         if(Math.random() > .5){
-            document.getElementById("coin").src = "assets/images/heads.png" //place heads coin image path here
+            document.getElementById("coin").src = "assets/images/heads.svg" //place heads coin image path here
             heads ++;
             flips ++;
             document.getElementById("heads").innerHTML = heads;
-            document.getElementById("total-flips").innerHTML = flips;;
+            document.getElementById("total-flips").innerHTML = flips;
         }
         else {
-            document.getElementById("coin").src ="assets/images/tails.png" //place tails coin image path here
+            document.getElementById("coin").src ="assets/images/tails.svg" //place tails coin image path here
             tails ++;
             flips ++;
             document.getElementById("tails").innerHTML = tails;
@@ -57,16 +58,21 @@ function coinToss (){
 
     //toggle button
 
-     document.getElementById("button").src ="assets/images/btn-in.png";
+     document.getElementById("button").src ="assets/images/main-button-in.svg";
 
 }
 
-
-//restart everything
+//restart everything - Click handler located in HTML
 
 function restart(){
+
     document.location.href = "";
+    sound2.play();
+
 }
+
+
+
 
 
 
